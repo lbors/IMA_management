1732a1087475b4db0e28e3d73894d07da1ea7fcf
williamgdo <williamgdo.app@gmail.com>
2019-05-02 22:26:41 -0300

    CRUD de services criado

--- engine_controller/engine_controller.py
+++ engine_controller/engine_controller.py
@@ -75,7 +75,7 @@ def create_pod():
     # -d@{arquivo}.json 
 
     resp = requests.post("http://" + master_ip + ":" + str(port) + "/api/v1/namespaces/" + data['podInfo']['namespace'] 
-                            + "/pods/", data = json.dumps(json_content['podInfo']['yaml_creation']))
+                            + "/pods/", data = json.dumps(json_content['podInfo']['pod_creation']))
     return str(resp.status_code)
 
 
@@ -101,8 +101,87 @@ def delete_pod():
 
     return str(resp.status_code)
 
+@app.route('/listServices', methods = ['POST']) 
+def list_services():
+    # ler arquivo de parametro
+    file_name = request.data.decode('utf-8')
+    file = open(file_name, "r")
+    yaml_content = file.read()
+    file.close()
+    data = yaml.safe_load(yaml_content) # parsear pra yaml
+
+    resp = requests.get("http://" + master_ip + ":" + str(port) + "/api/v1/namespaces/" + data['podInfo']['namespace'] + "/services/")
+    parsed = json.loads(resp.content)
+    print(json.dumps(parsed, indent=2))
+
+    return str(resp.status_code)
+
+@app.route('/createService', methods = ['POST'])
+def create_service():
+    file_name = request.data.decode('utf-8')
+    print(file_name)
+
+    # ler arquivo de parametro
+    file = open(file_name, "r")
+    yaml_content = file.read()
+    file.close()
+
+    # carrega o YAML, "parseia" pra Json 
+    data = yaml.load(yaml_content)
+    json_content = json.dumps(data)
+    json_content = json.loads(json_content)
+
+    resp = requests.post("http://" + master_ip + ":" + str(port) + "/api/v1/namespaces/" + data['podInfo']['namespace'] 
+                            + "/services/", data = json.dumps(json_content['podInfo']['service_creation']))
+    parsed = json.loads(resp.content)
+    print(json.dumps(parsed, indent=2))
+    return str(resp.status_code)
+
+@app.route('/getService', methods = ['POST'])
+def get_service():
+    file_name = request.data.decode('utf-8')
+    print(file_name)
+
+    # ler arquivo de parametro
+    file = open(file_name, "r")
+    yaml_content = file.read()
+    file.close()
+    data = yaml.load(yaml_content)
+
+    resp = requests.get("http://" + master_ip + ":" + str(port) + "/api/v1/namespaces/" + data['podInfo']['namespace'] + "/services/" + data['podInfo']['name'])
+
+    parsed = json.loads(resp.content)
+    print(json.dumps(parsed, indent=2))
+    return str(resp.status_code)
+
+
+@app.route('/deleteService', methods = ['DELETE']) 
+def delete_service():
+    file_name = request.data.decode('utf-8')
+    print(file_name)
+
+    # ler arquivo de parametro
+    file = open(file_name, "r")
+    yaml_content = file.read()
+    file.close()
+
+    # carrega o YAML, "parseia" pra Json 
+    data = yaml.load(yaml_content)
+    json_content = json.dumps(data)
+    json_content = json.loads(json_content)
+
+    resp = requests.delete("http://" + master_ip + ":" + str(port) + "/api/v1/namespaces/" + data['podInfo']['namespace'] + "/services/" + data['podInfo']['name'])
+
+    parsed = json.loads(resp.content)
+    print(json.dumps(parsed, indent=2))
+
+    return str(resp.status_code)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
 
 #TODO melhorar leitura
 #- deve poder criar yamls em sequencia
