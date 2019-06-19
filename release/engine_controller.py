b8a77a107e9c23101e620876cfa4c532c273869f
williamgdo <williamgdo.app@gmail.com>
2019-06-19 15:12:00 -0300

    yamls arrumados, novo dict em fase de desenvolvimento

--- release/engine_controller.py
+++ release/engine_controller.py
@@ -10,8 +10,6 @@ from subprocess import call
 
 app = Flask(__name__)
 adapter_dict = {"adapters":[]}
-# port_server = 8080
-# master_ip = '192.168.1.151'
 
 @app.route('/deleteService', methods = ['POST']) 
 def delete_service():
@@ -34,29 +32,16 @@ def delete_service():
                   return 'OK'
     return 'Adapter not found'
 
-# slice_id, slice_part_id e namespace sao passados como argumentos
-# @app.route('/listPods', methods = ['POST']) 
-# def list_pods():
-#     post_data = request.data.decode('utf-8') # exemplo de data: "Telemarketing;slice-part-test-01;espaco-testes"
-#     splitted_data = post_data.split(';')    
-#     # print(data)
-
-#     for adapter_iterator in adapter_dict['adapters']:
-#         if adapter_iterator['slice_id'] == splitted_data[0]:
-#             for slice_part_it in adapter_iterator['parts']:
-#                 if slice_part_it['slice_part_id'] == splitted_data[1]:
-#                     resp = requests.post("http://0.0.0.0:" + slice_part_it['port'] + "/listPods", data = post_data)
-#                     parsed = json.loads(resp.content)
-#                     print(json.dumps(parsed, indent=2))
-#                     return 'OK'
-#     # resp = requests.get("http://" + master_ip + ":" + str(port) + "/api/v1/namespaces/" + data['namespace'] + "/pods/")
-#     return 'Adapter not found'
-
 def save_dict():
     #filename = ""
     write_file = open("global_dict.json", 'w')
     json.dump(adapter_dict, write_file)
     write_file.close() 
+
+def load_dict_NEW():
+    jstring = ''.join(open('global_dict.json', 'r').readlines())
+    d = {i['slice_id']: {j['slice_part_id']: ({'adapter_name': j['adapter_name'], 'port': j['port']}) for j in i['parts']} for i in json.loads(jstring)['adapters']}
+    return d
 
 @app.before_first_request
 def load_dict():
@@ -71,9 +56,6 @@ def load_dict():
         print ('File "global_dict.json" does not exist. Resuming execution.')   
     except AttributeError: 
         print ('File "global_dict.json" is not a valid Json file. Resuming execution with a empty dict.')  
-
-
-
 
 @app.route('/createService', methods = ['POST']) 
 def create_service():
@@ -109,13 +91,12 @@ def start_slice_adapter(json_content):
         slice_name = i['name']
         slice_user = i['user']
         agent_name = slice_name + '_' + slice_user + '_agent'
-        # print ('Nome da slice:' + str(slice_name))
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(('localhost', 0))
         port = s.getsockname()[1]
         s.close()
-        # SALVAR: slice_part_id, slice port
+
         ######## slice-part-test-01, espaco-teste, nginx -> CHAMADA DE UM /getPod
         for j in i['vdus']: 
             if str(j['dc-vdu']['type']) == "master":
@@ -128,12 +109,9 @@ def start_slice_adapter(json_content):
         time.sleep(3)
         master_data = temp_ip + ":" + temp_port
         for k in adapter_dict['adapters']:
-            # print(k)
             if k['slice_id'] == json_content['slice-id']:
                 k['parts'].append({"slice_part_id":slice_name,"adapter_name":agent_name,"port":str(port)})
         # adapter_dict["adapters"].append({"slice_part_id":slice_name,"adapter_name":agent_name,"port":str(port)})
-
-        # print(json.dumps(adapter_dict, indent=2))
 
         requests.post("http://0.0.0.0:" + str(port) + "/setIPandPort", data = master_data)
         print("The Adapter", agent_name, "has started")
@@ -217,8 +195,8 @@ def stop_management():
     return '400'
 
 
-def stop_ma():
-    return 'Stopping the Resource and VM Management infrastructure'
+# def stop_ma():
+#     return 'Stopping the Resource and VM Management infrastructure'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5001')
@@ -228,10 +206,19 @@ if __name__ == '__main__':
 
 #TODO
 #- perguntar sobre retorno (a resposta eu que configuro? tem como voltar tanto uma resposta como um numero)
-#- fazer arquivo global_dict ficar invisivel ao usuario (e read only???)
+#- fazer arquivo global_dict ficar invisivel ao usuario (e read only???) [CONTRA: se fizer isso e deletar o adapter manualmente, programa morre]
 #- mudar chamadas de file-name pra file-content
+#- funcao "reset" (apagar dict)
 
 #TESTS
 #- verificar se podemos escolher pra qual worker o serviço vai
 #- testar /createService em dois VIMs (2 workers, 2 masters diferentes)
 #- começar update (orientado á tecnologia)
+
+""" jstring = ''.join(open('example_dict.json', 'r').readlines())
+aqui vc le o json como string
+d = {i['slice_id']: {j['slice_part_id']: (j['adapter_name'], j['port']) for j in i['parts']} for i in json.loads(jstring)['adapters']}
+aqui vc le e constrói como um dict
+
+aí é só acessar assim: d['Claro']['slice-02']
+eu não sei o que vai ser chave na sua busca, mas aí é só mudar no código (posso explicar melhor como, se vc quiser) """
