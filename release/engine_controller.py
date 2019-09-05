4da47ee2264f29d46d2b43d9e959025cb1aecebd
williamgdo <williamgdo.app@gmail.com>
2019-09-05 17:55:25 -0300

    mudancas no startManagement

--- release/engine_controller.py
+++ release/engine_controller.py
@@ -57,7 +57,13 @@ def start_slice_adapter(json_content):
             
             client = docker.from_env()
             client.containers.run("adapterk8s:latest", detach=True, name=agent_name, ports={'1010/tcp': ('localhost', port)})
-            time.sleep(3)
+            
+            while True:
+                try:
+                    requests.post("http://0.0.0.0:" + str(port) + "/setIPandPort", data = master_data)
+                    break
+                except requests.exceptions.ConnectionError:
+                    pass
 
             if json_content['slice']['id'] in adapter_dict:
                 adapter_dict[json_content['slice']['id']].update({ 
@@ -74,7 +80,6 @@ def start_slice_adapter(json_content):
                     }
                 })
             # print("http://0.0.0.0:" + str(port) + "/setIPandPort")
-            requests.post("http://0.0.0.0:" + str(port) + "/setIPandPort", data = master_data)
             print("The Adapter", agent_name, "has started")
         else:
             if json_content['slice']['id'] in adapter_dict:
@@ -109,7 +114,7 @@ def start_slice_adapter_ssh(json_content):
 
             for j in range(len(json_content['slice']['slice-parts'][i]['dc-slice-part']['VIM']['vdus'])): 
             # for para identificar o master sequencialmente
-                print(str(json_content['slice']['slice-parts'][i]['dc-slice-part']['VIM']['vdus'][j]))
+                # print(str(json_content['slice']['slice-parts'][i]['dc-slice-part']['VIM']['vdus'][j]))
                 if str(json_content['slice']['slice-parts'][i]['dc-slice-part']['VIM']['vdus'][j]['vdu']['type']) == "master": # um campo type identifica o mestre 
                     master_ip = str(json_content['slice']['slice-parts'][i]['dc-slice-part']['VIM']['vdus'][j]['vdu']['ip']) 
 
@@ -120,7 +125,13 @@ def start_slice_adapter_ssh(json_content):
 
             client = docker.from_env()
             client.containers.run("adapter_ssh:latest", detach=True, name=agent_name, ports={'1010/tcp': ('localhost', port)})
-            time.sleep(3)
+            
+            while True:
+                try:
+                    requests.post("http://0.0.0.0:" + str(port) + "/setSSH", data = master_data)
+                    break
+                except requests.exceptions.ConnectionError:
+                    pass
 
             if json_content['slice']['id'] in adapter_dict:
                 adapter_dict[json_content['slice']['id']].update({ 
@@ -151,7 +162,6 @@ def start_slice_adapter_ssh(json_content):
             
             master_data = ssh_ip + ":" + str(ssh_port) + ":" + ssh_user + ":" + ssh_pass + ":" + str(port) + ":" + master_ip
             # print("http://0.0.0.0:" + str(port) + "/setSSH")
-            requests.post("http://0.0.0.0:" + str(port) + "/setSSH", data = master_data)
             print("The Adapter", agent_name, "has started")
         else:
             if json_content['slice']['id'] in adapter_dict:
@@ -177,30 +187,104 @@ def default_options():
 def list_adapters():
     # print(json.dumps(adapter_dict, indent=2))
     return str(json.dumps(adapter_dict, indent=2))
-    
-# FUNCAO INCOMPLETA, PRECISA DE REVISAO    
-@app.route('/updateManagement', methods = ['POST'])
+
+def create_container(slice_name, slice_part, adapter_type, json_content):
+    # SSH OU API
+    print(str(json.dumps(json_content, indent=2)))
+
+    if adapter_type == "ssh":
+        agent_name = slice_part + '_adapter_ssh'
+        ssh_ip = json_content['dc-slice-part']['VIM']['vim-ref']['ip-ssh']
+        ssh_port = json_content['dc-slice-part']['VIM']['vim-ref']['port-ssh']
+        ssh_user = json_content['dc-slice-part']['VIM']['vim-credential']['user-ssh']
+        ssh_pass = json_content['dc-slice-part']['VIM']['vim-credential']['password-ssh']
+        # json_content['slice']['slice-parts'][i]
+
+        master_ip = "null"
+
+        for j in range(len(json_content['dc-slice-part']['VIM']['vdus'])): 
+        # for para identificar o master sequencialmente
+            # print(str(json_content['slice']['slice-parts'][i]['dc-slice-part']['dc-slice-part']['VIM']['vdus'][j]))
+            if str(json_content['dc-slice-part']['VIM']['vdus'][j]['vdu']['type']) == "master": # um campo type identifica o mestre 
+                master_ip = str(json_content['dc-slice-part']['VIM']['vdus'][j]['vdu']['ip']) 
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('localhost', 0))
+        port = s.getsockname()[1]
+        s.close()
+
+        client = docker.from_env()
+        client.containers.run("adapter_ssh:latest", detach=True, name=agent_name, ports={'1010/tcp': ('localhost', port)})
+
+        master_data = ssh_ip + ":" + str(ssh_port) + ":" + ssh_user + ":" + ssh_pass + ":" + str(port) + ":" + master_ip
+
+        while True:
+            try:
+                requests.post("http://0.0.0.0:" + str(port) + "/setSSH", data = master_data)
+                break
+            except requests.exceptions.ConnectionError:
+                pass
+
+        print("The Adapter", agent_name, "has started")
+
+        if slice_name in adapter_dict:
+            adapter_dict[slice_name].update({ 
+                    slice_part: ({
+                        "port":str(port),
+                        "adapter_ssh_name": agent_name,
+                        'ssh_ip': str(ssh_ip),
+                        'ssh_port': str(ssh_port),
+                        'ssh_user': str(ssh_user),
+                        'ssh_pass': str(ssh_pass),
+                        'master_ip': str(master_ip)
+                    })
+            })
+        else:
+            adapter_dict.update({
+                slice_name: {
+                    slice_part: ({
+                        'port': str(port),
+                        'adapter_ssh_name': agent_name,
+                        'ssh_ip': str(ssh_ip),
+                        'ssh_port': str(ssh_port),
+                        'ssh_user': str(ssh_user),
+                        'ssh_pass': str(ssh_pass),
+                        'master_ip': str(master_ip)
+                    })
+                }
+            })
+
+def delete_container(slice_name, slice_part, container_name):
+    client = docker.from_env()
+    container = client.containers.get(container_name)
+    container.stop()
+    container.remove()
+    adapter_dict[slice_name].pop(slice_part)
+
+@app.route('/necos/ima/updateManagement', methods = ['POST'])
 def update_management():
     # carrega o YAML e "parseia" pra Json  
     data = yaml.safe_load(request.data.decode('utf-8'))
-    json_content = json.dumps(data)
+    json_content = json.dumps(data) 
     json_content = json.loads(json_content)
 
-    if json_content['flag'] == "append":
-        start_slice_adapter(json_content)
+    if json_content['slice']['id'] in adapter_dict:
+        i = 0
+        for i in range(len(json_content['slice']['slice-parts'])):
+            slice_name = json_content['slice']['slice-parts'][i]['dc-slice-part']['name']
+            if slice_name in adapter_dict[json_content['slice']['id']]:
+                # print("Deleting adapter '" + slice_name + "_adapter_api" + "'")
+                # delete_container(json_content['slice']['id'], slice_name, slice_name + "_adapter_api")
+                print("Deleting adapter '" + slice_name + "_adapter_ssh" + "'")
+                delete_container(json_content['slice']['id'], slice_name, slice_name + "_adapter_ssh")
+            else:
+                # start_slice_adapter(json_content)
+                print("Creating adapter '" + slice_name + "_adapter_ssh" + "'")
+                create_container(json_content['slice']['id'], slice_name, "ssh", json_content['slice']['slice-parts'][i])
         save_dict()
         return str(json.dumps(adapter_dict, indent=2))
-    elif json_content['flag'] == "delete":  
-        client = docker.from_env()
-        container = client.containers.get(str(json_content['slice-part-id']))
-        container.stop()
-        container.remove()
-        adapter_dict[json_content['slice-id']].pop(json_content['slice-part-id'])
-        save_dict()
-        return str(json.dumps(adapter_dict, indent=2))
-    else: 
-        return 'Error: The yaml sent has a invalid flag.'
-    
+    else:
+        return "Error: The slice ID in the yaml doesn't exists."
 
 @app.route('/necos/ima/start_management', methods = ['POST'])
 def start_management():
@@ -224,7 +308,7 @@ def stop_management():
         #     container.stop()
         #     container.remove()
         if adapter_dict[post_data][slice_part]["adapter_ssh_name"] != "null":
-            container = client.containers.get(adapter_dict[slice_part]["adapter_ssh_name"])
+            container = client.containers.get(adapter_dict[post_data][slice_part]["adapter_ssh_name"])
             container.stop()
             container.remove()
     adapter_dict.pop(post_data)
